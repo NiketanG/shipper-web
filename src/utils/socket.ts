306aca212128430bcacfd233ca0ab7d070b97802
Niketan Gulekar <nikegulekar@gmail.com>
2021-01-24 00:58:12 +0530

    Update socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_API_URL, {
+const socket = io(process.env.REACT_APP_API_URL || "http://localhost:4000/", {
 	withCredentials: true,
 });
 
